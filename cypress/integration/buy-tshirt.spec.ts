94d6ebe119d264022849186e4bee1ba5d7889b9c
ghporras@gmail.com <dG26ZeK6tyRP>
2020-02-12 13:50:36 -0500

    feat: setup cypress configuration part 8 - updating describe test with friendlier nested describes

--- cypress/integration/buy-tshirt.spec.ts
+++ cypress/integration/buy-tshirt.spec.ts
@@ -1,45 +1,75 @@
 import { AddressStepPage, HomePage, MenuContentPage, PaymentStepPage, ProductAddedModalPage,
          ProductListPage, OrderSummaryStepPage, ShippingStepPage, SignInStepPage } from '../page';
 
-describe('Buy a t-shirt', () => {
-  const addressStepPage: AddressStepPage = new AddressStepPage();
-  const homePage: HomePage = new HomePage();
-  const menuContentPage: MenuContentPage = new MenuContentPage();
-  const orderSummaryPage: OrderSummaryStepPage = new OrderSummaryStepPage();
-  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
-  const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
-  const productListPage: ProductListPage = new ProductListPage();
-  const shippingPage: ShippingStepPage = new ShippingStepPage();
-  const signInPage: SignInStepPage = new SignInStepPage();
+describe('Given an user', () => {
 
-  it('Then should be bought a t-shirt', () => {
-    // HomePage
-    homePage.visit();
+  describe('When he visit our e-commerce', () => {
+    before(() => {
+      const homePage: HomePage = new HomePage();
 
-    // Navigation in the products
-    menuContentPage.mainMenuItemTShirtClick();
-    productListPage.addToCartClick();
-    productAddedModalPage.proceedToCheckoutButtonClick();
+      homePage.visit();
+    });
 
-    // Begin buying
-    orderSummaryPage.proceedToCheckoutButtonClick();
+    describe('And select a t-shirt', () => {
+      before(() => {
+        const menuContentPage: MenuContentPage = new MenuContentPage();
 
-    // Sign in
-    signInPage.emailTextBoxFill('aperdomobo@gmail.com');
-    signInPage.passwordTextBoxFill('WorkshopProtractor');
-    signInPage.signInClick();
+        menuContentPage.mainMenuItemTShirtClick();
+      });
 
-    // Continue process
-    addressStepPage.proceedToCheckoutButtonClick();
+      describe('And add it to the cart to buy it', () => {
+        before(() => {
+          const productListPage: ProductListPage = new ProductListPage();
 
-    // Shipping address
-    shippingPage.iAgreeTheTermsCheck();
-    shippingPage.proceedToCheckoutClick();
+          productListPage.addToCartClick();
+        });
 
-    // Payment
-    paymentStepPage.payByBankWireClick();
-    paymentStepPage.iConfirmMyOrderClick();
-    paymentStepPage.yourOrderIsCompleteObj()
-      .should('have.text', 'Your order on My Store is complete.');
+        describe('And proceed to the buying', () => {
+          before(() => {
+            const productAddedModalPage: ProductAddedModalPage = new ProductAddedModalPage();
+            const orderSummaryPage: OrderSummaryStepPage = new OrderSummaryStepPage();
+
+            productAddedModalPage.proceedToCheckoutButtonClick();
+            orderSummaryPage.proceedToCheckoutButtonClick();
+          });
+
+          describe('And authenticate', () => {
+            before(() => {
+              const signInPage: SignInStepPage = new SignInStepPage();
+
+              signInPage.emailTextBoxFill('aperdomobo@gmail.com');
+              signInPage.passwordTextBoxFill('WorkshopProtractor');
+              signInPage.signInClick();
+            });
+
+            describe('And finalize the buying process using bank wire payment', () => {
+              before(() => {
+                const addressStepPage: AddressStepPage = new AddressStepPage();
+                const shippingPage: ShippingStepPage = new ShippingStepPage();
+                const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+
+                addressStepPage.proceedToCheckoutButtonClick();
+                shippingPage.iAgreeTheTermsCheck();
+                shippingPage.proceedToCheckoutClick();
+                paymentStepPage.payByBankWireClick();
+              });
+
+              it('then he should get the order finished', () => {
+                const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+
+                paymentStepPage.iConfirmMyOrderClick();
+                paymentStepPage.yourOrderIsCompleteObj()
+                  .should('have.text', 'Your order on My Store is complete.');
+              });
+            });
+          });
+
+        });
+
+      });
+
+    });
+
   });
+
 });
